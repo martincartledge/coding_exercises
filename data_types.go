ed20ff3a9e36c3ba33a0798324a5fc87459e7a47
Martin Cartledge <mcartledge@gogoair.com>
2020-09-05 07:24:44 -0600

    update code to allow all tests to pass

--- data_types.go
+++ data_types.go
@@ -10,25 +10,37 @@ import (
 func main() {
 	var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
 
-	var i = 4
+	var i uint64 = 4
 	var d = 4.0
 	var s = "HackerRank "
 
 	scanner := bufio.NewScanner(os.Stdin)
 	// Declare second integer, double, and String variables.
-	var num = 12
-	var dub = 4.0
-	var str = "is the best place to learn and practice coding!"
+	scanner.Scan()
+
+	j, _ := strconv.ParseUint(scanner.Text(), 0, 64)
+
+	scanner.Scan()
+
+	e, _ := strconv.ParseFloat(scanner.Text(), 64)
+
+	scanner.Scan()
+
+	var t string = scanner.Text()
+
+	fmt.Println(i + j)
+
+	fmt.Printf("%.1f", d+e)
+
+	fmt.Println(s + t)
 
 	// Read and save an integer, double, and String to your variables.
 	// Print the sum of both integer variables on a new line.
-	var sum = num + i
-	fmt.Println(sum)
+
 	// Print the sum of the double variables on a new line.
-	var dSum = dub + d
-	fmt.Printf("%.1f \n", dSum)
+
 	// Concatenate and print the String variables on a new line
-	fmt.Println(s + str)
+
 	// The 's' variable above should be printed first.
 	scanner.Scan()
 }
