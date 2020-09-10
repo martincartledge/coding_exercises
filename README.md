c7543bac843e6ebdd2010cbcced362c4200ab233
Martin Cartledge <mcartledge@gogoair.com>
2020-09-10 07:35:14 -0600

    complete find palindrome exercises

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Collection of coding challenges completed from [The Daily Byte](https://thedailybyte.dev/)
 
 - [x] Reverse String (es5 manual, es6 manual, es6 helper methods)
+- [x] Find Palindrome (es5, es6)
---
+++ palindrome.js
@@ -0,0 +1,29 @@
+// es5
+function pali(input) {
+  const arr = input.split("");
+  const revArr = [];
+  for (let i = 1; i < arr.length + 1; i++) {
+    revArr.push(arr[arr.length - i]);
+  }
+  const reversed = revArr.join("");
+  const rev = reversed.replace(/[^0-9a-zA-Z]/g, "").toLowerCase();
+  const inp = input.replace(/[^0-9a-zA-Z]/g, "").toLowerCase();
+  return inp === rev;
+}
+
+// es6
+const pali = (input) => {
+  const arr = input.split("");
+  const revArr = [];
+  for (let i = 1; i < arr.length + 1; i++) {
+    revArr.push(arr[arr.length - i]);
+  }
+  const reversed = revArr.join("");
+  const rev = reversed.replace(/[^0-9a-zA-Z]/g, "").toLowerCase();
+  const inp = input.replace(/[^0-9a-zA-Z]/g, "").toLowerCase();
+  return inp === rev;
+};
+
+pali("level");
+pali("algorithm");
+pali("A man, a plan, a canal: Panama.");
